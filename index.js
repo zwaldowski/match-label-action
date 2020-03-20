e7521f7133f86551436e321f8dd3974ddced714d
Max Desiatov <max@desiatov.com>
2020-03-20 21:13:49 +0000

    Implement allowed_multiple input with tests

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ function run() {
     const labels = pr.labels || []
     const labelNames = labels.map(label => label.name)
     const allowedLabels = match.parseAllowed(core.getInput('allowed'))
-    const matchingLabel = match.findMatching(labelNames, allowedLabels)
-    core.setOutput('match', matchingLabel)
+    const allowedMultipleLabels = match.parseAllowed(core.getInput('allowed_multiple'))
+    let matchingLabel;
+    if (allowedLabels.length > 0) {
+      matchingLabel = match.findMatching(labelNames, allowedLabels, false)
+    } else if (allowedMultipleLabels.length > 0) {
+      matchingLabel = match.findMatching(labelNames, allowedMultipleLabels, true)
+    } else {
+      return core.setFailed('You must provide either `allowed` or `allowed_multiple` as input.')
+    }
+    core.setOutput('match', matchingLabel.join(', '))
   } catch (error) {
     core.setFailed(error.message)
   }
