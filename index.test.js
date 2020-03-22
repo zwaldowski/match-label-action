20669196f235fc068e68137a9c34af76e8c5ce62
Zachary Waldowski <zach@waldowski.me>
2020-03-22 15:06:21 -0400

    Update dependencies

--- index.test.js
+++ index.test.js
@@ -6,5 +6,5 @@ test('test runs', () => {
     process.env.GITHUB_EVENT_PATH = path.join(__dirname, '.tests/context.json')
     const ip = path.join(__dirname, 'index.js')
     const output = cp.execSync(`node ${ip}`, {env: process.env, encoding: 'utf8'})
-    expect(output).toBe('::set-output name=match,::hello\n')
+    expect(output).toBe('::set-output name=match::hello\n')
 })
