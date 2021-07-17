4b26ac769bcce67ea0bdea26f9f7838a3c8a6918
Stefan <stefan.astrand@unitar.io>
2021-07-17 12:42:16 +0200

    Debugging issue with escaped characters not working with parseAllowed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const match = require('./match')
 function run() {
   try {
     const pr = context.payload.pull_request || {}
-    const labels = pr.labels || context.labels || []
+    const labels = pr.labels || []
     const labelNames = labels.map(label => label.name)
     const allowedLabels = match.parseAllowed(core.getInput('allowed'))
     const allowedMultipleLabels = match.parseAllowed(
